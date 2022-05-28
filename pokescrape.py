84cd321fa6a87de768ba6b9a00cf03c393d92d6e
Kenneth Carroll <kenneth.james.carroll@gmail.com>
2022-05-28 11:27:51 -0400

    Updated to reflect the current state of pokemon and accomodate multiple forms

--- pokescrape.py
+++ pokescrape.py
@@ -1,14 +1,35 @@
 import urllib.request
 import time
+import os
 
-start_time = time.time()
+def retrieve_pokemon_image(file, url):
+    cwd = os.getcwd()
+    fullfilename = os.path.join(cwd,'images', file)
+    print(fullfilename)
+    try:
+        urllib.request.urlretrieve(url, fullfilename)
+        print("Saved ",  file)
+    except:
+        print("The attempted pokemon does not exist in Pokemon Assets")
 
-for i in range (1, 806):
-    file_name =  '{:03d}'.format(i) + ".png"
-    url_string = "https://assets.pokemon.com/assets/cms2/img/pokedex/full/" + file_name
-    urllib.request.urlretrieve(url_string, file_name)
-    print("Saved ",  file_name)
+def main():
+    start_time = time.time()
+    # Latest Number Available in Pokemon Online
+    for i in range (1, 906):
 
-end_time = time.time()
-print("Done")
-print("Time Duration = ", end_time - start_time, "sec")
+        file_name =  '{:03d}'.format(i) + ".png"
+        url_string = "https://assets.pokemon.com/assets/cms2/img/pokedex/full/" + file_name
+        retrieve_pokemon_image(file_name, url_string)
+
+        # Number of variations like mega/gigantamax/hisuian
+        for x in range(2,6):
+            file_name =  '{:03d}'.format(i) + "_f" + str(x) + ".png"
+            url_string = "https://assets.pokemon.com/assets/cms2/img/pokedex/full/" + file_name
+            retrieve_pokemon_image(file_name, url_string)
+
+    end_time = time.time()
+    print("Done")
+    print("Time Duration = ", end_time - start_time, "sec")        
+
+if __name__ == "__main__":
+    main()
